d9b61fe79cc8e2364f03dac4257c01da98cf7555
qipbbn <qipbbn@gmail.com>
2013-02-02 23:15:10 +0800

    update plugin mananger

---
+++ test/plugin.test.js
@@ -0,0 +1,32 @@
+var plugin = require('../lib/plugin');
+require('should');
+
+describe("PluginManager Test", function(){
+
+    describe("add plugin", function(){
+        var pm;
+        var abcObj = {};
+        var initConfig = {};
+
+        var p1 = function(abc, initConfig){
+            abc.should.eql(abcObj, 'plugin abc param');
+            initConfig.should.eql(initConfig, 'initConfig should passed in');
+            return function(runtime, done){
+                runtime.foo.should.eql('bar');
+                done();
+            }
+        };
+
+        before(function() {
+            pm = new plugin.PluginManager(abcObj);
+        });
+
+        it('should work with function', function () {
+            pm.add('foo', p1, initConfig);
+        });
+
+        it('should exec the foo plugin with configs', function(done){
+            pm.exec('foo', {foo:'bar'}, done);
+        });
+    });
+});
